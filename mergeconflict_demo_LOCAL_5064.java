4477d8b173b1353db70f10d144c973a2c39addbc
shravanrachakonda <rshravan02@gmail.com>
2018-08-16 20:58:33 +0530

    adding the untracked files to the mergebranch2

---
+++ mergeconflict_demo_LOCAL_5064.java
@@ -0,0 +1,9 @@
+public class MergeConflictDemo1{
+
+public static void main(String args[]){
+
+	System.out.println("Merge conflict demo1.java file creaeted in branch1");
+
+}
+
+}
---
+++ mergeconflict_demo_REMOTE_5064.java
@@ -0,0 +1,10 @@
+public class MergeConflictDemo{
+public static void main(String args[]){
+
+
+	System.out.println("created a file in mergebranch2 for mergeconflict");
+	System.out.println("my sencond line in demo java file");
+}
+
+
+}
