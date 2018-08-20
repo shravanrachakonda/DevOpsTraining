96861bb49bb5ab221535d4a4d70057fff9fc8672
shravanrachakonda <42149217+shravanrachakonda@users.noreply.github.com>
2018-08-20 22:04:02 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -3,7 +3,7 @@ public class Test{
 public static void main(String args[])
 {
 
-	System.out.println("My deveops Training");
+	System.out.println("My deveops Training - build afte commit");
 	System.out.println("after mergetool kdiff3 adding a line for merging to master branch");
 }
 
